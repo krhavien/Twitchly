65ee496176887fd18b7aee2bba08d97c0e2b10a2
scottnuma <scott.numamoto@gmail.com>
2018-10-21 11:10:29 -0700

    Add refreshing of existing users

--- web/twitchly_db.py
+++ web/twitchly_db.py
@@ -86,3 +86,19 @@ class Database:
         combined["follows"] = [c['id'] for c in twitch_user.get_all_follows(id)]
 
         return combined
+
+    def all_user_ids(self):
+        """Return a list of all user ids in the database."""
+        ref = db.reference('users')
+        snapshot = ref.get()
+        return snapshot.keys() 
+
+    def refresh_all_user_info(self):
+        """Refreshes the information of all people in the database."""
+        user_ids = self.all_user_ids()
+        for user_id in user_ids:
+            self.get_user_info(user_id, force_online=True)
+            logging.info("updated user id %s", user_id)
+
+
+
