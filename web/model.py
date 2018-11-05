d113da7116f4f3ed3249fc1fc37827452608e717
Aditya Varshney <adityavarshney@gmail.com>
2018-11-05 11:51:03 -0800

    updated preprocessing docs, added normalization, comments

--- web/model.py
+++ web/model.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 
 SAMPLE_FILE_NAME = 'channels_sample_2000.csv'
 
@@ -9,9 +10,9 @@ SAMPLE_FILE_NAME = 'channels_sample_2000.csv'
 class KMeansModel:
 	""" Creates a K-Means Clustering model that uses unsupervised learning to cluster records in featurized dataframes """
 
-	def __init__(self, n_clusters):
+	def __init__(self, n_clusters=15):
 		self.n_clusters = n_clusters
-		self.model = KMeans(n_clusters=15, random_state=14)
+		self.model = KMeans(n_clusters=n_clusters, random_state=14)
 		self.data = None
 
 	def train(self, input_data=None, assign_clusters=False):
@@ -27,7 +28,7 @@ class KMeansModel:
 			X = pd.read_csv(SAMPLE_FILE_NAME, encoding='ISO-8859-1')
 		
 		X = self.preprocess(X)
-		self.model.fit(X.select_dtypes(include=['int64']))
+		self.model.fit(X.select_dtypes(include=['int64','float64']))
 		
 		if assign_clusters:
 			X['pred_cluster'] = self.predict(X)
@@ -41,10 +42,18 @@ class KMeansModel:
 		Returns: Predicted cluster
 		"""
 		X = self.preprocess(X)
-		return self.model.predict(X.select_dtypes(include=['int64']))
+		return self.model.predict(X.select_dtypes(include=['int64','float64']))
 
 
 	def preprocess(self, X):
+		"""
+		Converts X to a dataframe object ready for use by the K-Means Model
+		- Required columns: ['views', 'broadcaster_language', 'display_name', 'followers', 'game', 'language', 'id', 'follows']
+		- Featurizes X to include:
+			- game index: a number assigned to a particular game
+
+		Returns: new dataframe object with adjusted columns.
+		"""
 		if not isinstance(X, pd.DataFrame):
 			X = pd.DataFrame([X])
 		t = X.drop_duplicates(subset=['id'])
@@ -53,6 +62,10 @@ class KMeansModel:
 		t['followers'] = t['followers'].map(int)
 		t['views'] = t['views'].map(int)
 		t['game_idx'] = t['game'].map({game: num for num, game in enumerate(t['game'].unique())})
+
+		# normalize data using min/max scaling
+		scaler = MinMaxScaler()
+		t[['followers', 'views']] = scaler.fit_transform(t[['followers', 'views']])
 		return t
 
 def create_model(n_clusters=10):
@@ -62,29 +75,26 @@ def create_model(n_clusters=10):
 
 """
 SAMPLE CODE:
-
+"""
+"""
+# initialize db
 import twitchly_db
 try:
     db = twitchly_db.Database()
 except ValueError:
     db = db
-channel_id = '19571641' # ninja's channel id
 
-model = create_model()
+# ninja's channel id
+channel_id = '19571641' 
+
+# create model / predict
+model = create_model(n_clusters=10)
 model.train(assign_clusters=True)
-
 ninja_pred = model.predict(db.get_user_info(channel_id))
+
+# show results
 print('Predicted cluster for Ninja: ', ninja_pred)
 print('Similar matches')
 print(model.data[model.data['pred_cluster']==ninja_pred[0]][['display_name', 'followers']])
-
-OUTPUT (untuned model, trained on sample data):
-
-Predicted cluster for Ninja:  [9]
-Similar matches
-        display_name  followers
-0              Ninja   11884880
-73   BeyondTheSummit     716330
-136      Starladder1     476067
-
+print(model.data.groupby('pred_cluster').count().views)
 """
