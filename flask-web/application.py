e694f946cf76a6ad88ddbab5b044d8256bda5b64
Karl Cempron <cempronkarl@gmail.com>
2018-11-17 12:07:15 -0800

    Migrated model files to flask-web and integrated into app

--- flask-web/application.py
+++ flask-web/application.py
@@ -2,6 +2,7 @@ import logging
 import os
 import twitchly_db
 import twitch_user
+import model
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory)
@@ -49,6 +50,9 @@ def send_user():
     if username:
         user_id = twitch_user.get_user_id(username)
         user_info = database.get_user_info(user_id)
+        user_pred = rec_model.predict(user_info)
+        print(rec_model.data[rec_model.data['pred_cluster']==user_pred[0]][['display_name', 'followers']])
+        print(rec_model.data.groupby('pred_cluster').count().views)
         return render_template('user-profile.html', username=username, userinfo=user_info)
 
     return render_template('user-search.html')
@@ -58,6 +62,9 @@ def page_not_found(e):
     return render_template('404.html')
 
 if __name__ == "__main__":
+    rec_model = model.create_model(n_clusters=10)
+    print("model trained!")
+    rec_model.train(assign_clusters=True)
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     application.run()
