ccc6b48e98b906be00b60f20f4ffec39d03619ab
Aditya Varshney <adityavarshney@gmail.com>
2018-11-25 23:07:08 -0800

    updated sampling in data analysis rd2

--- web/model.py
+++ web/model.py
@@ -75,21 +75,47 @@ class KMeansModel:
 	def get_closest_matches(self, channel_id, num_suggestions=10):
 		"""
 		Finds closest matches
+
+		Returns dataframe with top k best matches, the predicted cluster, and the pruning filters used after cluster prediction
 		"""
-		if not model.data:
+		if len(model.data) == 0:
 			print("No data loaded into model. Training...")
 			self.train(assign_clusters=True)
+		# if not db:
+		# 	try:
+		# 		db = twitchly_db.Database()
+		# 	except ValueError:
+		# 		pass
 
-		try:
-		    db = twitchly_db.Database()
-		except ValueError:
-		    db = db
+		response = db.get_user_info(channel_id)
+		print(response['display_name'], response['followers'], response['game'], response['broadcaster_language'])
+		print(response['follows'])
 		
-		predicted_cluster = self.predict(db.get_user_info(channel_id))
-		matches = model.data[model.data['pred_cluster']==predicted_cluster[0]]
-		matches = matches.sample(n=10)
+		# run clustering
+		predicted_cluster = self.predict(response)
 
-		return matches
+		# create filters
+		filters = ['game', 'broadcaster_language']
+		filters_used = []
+		filter_dict = {fil: response[fil] for fil in filters}
+
+		# filter df
+		matches = model.data[
+			(model.data['pred_cluster']==predicted_cluster[0]) 
+			& (model.data['id'] != response['id'])]
+		for fil, val in filter_dict.items():
+			filtered_df = matches[matches[fil]==val]
+			# if len(filtered_df) > num_suggestions:
+			print('filtering by {}=={}'.format(fil, val))
+			filters_used.append((fil, val))
+			matches = filtered_df
+		
+		centroids = self.model.cluster_centers_
+		print(len(matches))
+		if len(matches) >= num_suggestions:
+			matches = matches.head(num_suggestions)
+		# print(model.data[model.data['followers']<.01]['id'].head())
+		return matches, predicted_cluster, filters_used
 
 def create_model(n_clusters=10):
 	""" External endpoint for creating a model capable of training and predicting clusters. """
@@ -99,25 +125,32 @@ def create_model(n_clusters=10):
 """
 SAMPLE CODE:
 """
-"""
+# """
 # initialize db
 import twitchly_db
 try:
     db = twitchly_db.Database()
 except ValueError:
-    db = db
+    print()
 
 # ninja's channel id
-channel_id = '19571641' 
+# channel_id = '19571641' 
+
+#xQcOW's channel id
+channel_id = '71092938'
+
+# bossLayf's channel id
+# channel_id = '195736337'
 
 # create model / predict
 model = create_model(n_clusters=10)
 model.train(assign_clusters=True)
-ninja_pred = model.predict(db.get_user_info(channel_id))
+
+top_10_hits, predicted_cluster, filters_used = model.get_closest_matches(channel_id)
 
 # show results
-print('Predicted cluster for Ninja: ', ninja_pred)
+print('Predicted cluster for User: ', predicted_cluster)
 print('Similar matches')
-print(model.data[model.data['pred_cluster']==ninja_pred[0]][['display_name', 'followers']])
-print(model.data.groupby('pred_cluster').count().views)
-"""
+print(top_10_hits[['display_name', 'followers']])
+# print(model.data.groupby('pred_cluster').count().views)
+# """
